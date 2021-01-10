cc68d4347a41643ee9a5be98d00f94170be09ef1
VPcenter <vpokhapkin@yandex.ru>
2021-01-11 01:58:36 +0300

    workable connection

--- app.py
+++ app.py
@@ -23,8 +23,7 @@ if __name__ == "__main__":
     
     try:
         client=MongoClient('mongodb://travis:test@127.0.0.1:27017/mydb_test')
-        db=client.mydb_test
-        logger.debug(f'{client.list_database_names()}')  
+        logger.opt(colors=True).debug(f'<m>{client.list_database_names()}</m>') 
     except:
         logger.opt(colors=True).debug(f'<y>No connection</y>')
 
